eb5a8dd21d36010db5d3e95aef7f43dffb5cb371
Ryan LaRue <28721348+Ryanmmlarue@users.noreply.github.com>
2022-09-01 11:33:20 -0400

    maybe?

--- src/features/NavigationBar/NavigationBar.tsx
+++ src/features/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,17 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavigationBar = () => {
 
   return (
     <Navbar bg="light" expand="lg">
         <Container>
-        <Navbar.Brand href="/#/home">Convex Hull</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/home">Convex Hull</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="/#/home">Home</Nav.Link>
-            <Nav.Link href="/#/aide">Application</Nav.Link>
+            <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/aide">Application</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Container>
